84917b1877d67a4a6f08d756a3546aa34999e571
astronomer80 <astronomer@tiscali.it>
2019-09-01 21:41:52 +0200

    UI Improvement

--- AndroidApp/Readme.md
+++
@@ -1,1 +0,0 @@
-Advertiser Android App
--- AndroidApp/app/src/main/java/org/astrosafesys/actuator_11/MainActivity.java
+++ AndroidApp/app/src/main/java/org/astrosafesys/actuator_11/MainActivity.java
@@ -1,10 +1,13 @@
-package org.astrosafesys.actuator;
+package org.astrosafesys.actuator_11;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
@@ -17,17 +20,21 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private static int TIMEOUT_TIME = 3000;
 
     BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
-    Switch switch_button;
+    Switch switch_button_gate;
+    Switch switch_button_garage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        switch_button = findViewById(R.id.Cancello);
-        switch_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switch_button_gate = findViewById(R.id.Cancello);
+        switch_button_gate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked)
@@ -37,27 +44,52 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        if (this.getApplicationContext().checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
-            this.requestPermissions( new String[]{Manifest.permission.BLUETOOTH}, 666);  // Comment 26
-            return;
-        }
-        if (this.getApplicationContext().checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
-            this.requestPermissions( new String[]{Manifest.permission.BLUETOOTH_ADMIN}, 666);  // Comment 26
-            return;
+        switch_button_garage = findViewById(R.id.Garage);
+        switch_button_garage.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    sendAdvertiseData(40);
+                //else
+                //    fResetSwitch();
+            }
+        });
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.getApplicationContext().checkSelfPermission(Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]{Manifest.permission.BLUETOOTH}, 666);  // Comment 26
+                return;
+            }
+            if (this.getApplicationContext().checkSelfPermission(Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(new String[]{Manifest.permission.BLUETOOTH_ADMIN}, 666);  // Comment 26
+                return;
+            }
         }
 
+    }
+
+    private boolean fCheckBluetoothDevice(){
+        boolean rRet=false;
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
         } else {
             if (!mBluetoothAdapter.isEnabled()) {
+
                 mBluetoothAdapter.enable();
                 // Bluetooth is not enable :)
-            }
+            }else
+                rRet=true;
+
         }
+        return rRet;
     }
 
     void sendAdvertiseData(int pDeviceID) {
+        if(!fCheckBluetoothDevice()) {
+            fResetSwitch();
+            return;
+        }
 
         AdvertiseSettings settings = new AdvertiseSettings.Builder()
                 .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
@@ -71,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
         byte[] rData = new byte[4];
         rData[0] = 29;  //Action
-        rData[1] = (byte) pDeviceID;  //ID
+        rData[1] = (byte)pDeviceID;  //ID
         rData[2] = (byte) 0xAB; //Postfix
         rData[3] = (byte) 0xAA; //Postfix
 
@@ -115,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
      */
     void fResetSwitch(){
         Log.i("BLE", "Stop Advertising");
-        switch_button.setChecked(false);
+        switch_button_gate.setChecked(false);
 
     }
 }
